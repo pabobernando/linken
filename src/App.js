ccc38172dfd8417eb440ed018f4b75e9b52197ee
Pabo Bernando <pabobernando69@gmail.com>
2023-07-19 08:54:36 +0700

    fixed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import './App.css';
+import React from 'react';
 import Home from './pages/Home';
 import Materi from './pages/Materi';
 import Navbar from './components/Navbar';
